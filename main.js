873b683b6166030c60ddebe551bfd7df7b439ba6
swanith123 <73668844+swanith123@users.noreply.github.com>
2022-02-24 19:13:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+array_1=["pen","paper","book", "bottle"];
+random_number = Math.floor((Math.random()*array_1.length)+1)
+console.log(random_number = quick_draw_data_set);
+quick_draw_data_set = array_1[random_number];
+document.getElementById.innerHTML = ("span_score" + "span_timer" = 'Sketch To Be Drawn: ' + drawn_sketch);
+
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
